64b295dc4a78ee5600042a3636e3be6280784e10
nico12450 <nicolas.gamel12450@gmail.com>
2021-10-24 11:11:53 +0200

    add step button

--- Renderer.py
+++ Renderer.py
@@ -4,6 +4,7 @@ from GameOfLife import *
 
 def drawGrid(grid: grid, cnv: Canvas):
 
+    cnv.delete('all')
     cnvWidth = cnv.winfo_reqwidth()
     wstep = cnvWidth/grid.width
     cnvHeight = cnv.winfo_reqheight()
@@ -17,15 +18,33 @@ def drawGrid(grid: grid, cnv: Canvas):
 
         cnv.create_line(0, j*hstep, cnvWidth, j*hstep)
 
+    for i in range(len(grid.grid)):
+        for j in range(len(grid.grid[i])):
+
+            if grid.get(j, i):
+
+                x = j * wstep
+                y = cnvHeight - (i * hstep)
+
+                cnv.create_rectangle(x, y, x + wstep, y + hstep, fill = 'black')
+
+def next(grid: grid, cnv: Canvas):
+
+    gameOfLife.nextState()
+    drawGrid(grid, cnv)
+
+
 root = Tk()
 
 frm = ttk.Frame(root, padding=10)
 frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+# ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+#ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
 
 cnv = Canvas(root, width=root.winfo_screenwidth()*0.8, height=root.winfo_screenheight()*0.8, bg="ivory")
 cnv.grid()
 drawGrid(gameOfLife.grid, cnv)
+
+ttk.Button(frm, text="Next", command=lambda: next(gameOfLife.grid, cnv)).grid(column=0, row=0)
 
 root.mainloop()
--- gameOfLife.py
+++ gameOfLife.py
@@ -1,9 +1,9 @@
 from Engine import *
 
 gameOfLife = gridGame([rule(False, 3, '=', 'born'), rule(True, 2, '<', 'die'), rule(True, 3, '>', 'die')])
-# gameOfLife.grid.setTrue(4, 4)
-# gameOfLife.grid.setTrue(5, 4)
-# gameOfLife.grid.setTrue(6, 4)
+gameOfLife.grid.setTrue(4, 4)
+gameOfLife.grid.setTrue(5, 4)
+gameOfLife.grid.setTrue(6, 4)
 # gameOfLife.display()
 # gameOfLife.nextState()
 # gameOfLife.display()
